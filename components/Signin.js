c43797f92e961c21ada0369b39fccf344a2c5290
johannes <jmuljana@gmail.com>
2021-11-03 09:35:22 +1100

    updated sign in

--- components/Signin.js
+++ components/Signin.js
@@ -1,15 +1,61 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from "react-native";
 import { useNavigation } from '@react-navigation/native';
-import { AuthForm } from './AuthForm';
+import { ThemeColours } from './ThemeColours';
 
 export function Signin ( props ) {
   const navigation = useNavigation()
   return(
-    <View>
-      <Text>Sign in</Text>
-      <AuthForm />
-      <Button title="Click here to sign up" onPress={ () => navigation.navigate("Signup") }/>
+    <View style={styles.container}>
+      <Text>Sign up</Text>
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+      <View style={styles.inner}>
+        <Text>Email</Text>
+        <TextInput style={styles.input} />
+        <Text>Password</Text>
+        <TextInput style={styles.input} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+        <Text>Don't have an account?</Text>
+        <Button title="Sign up for an account" onPress={() => navigation.navigate("Signup")} />
+      </View>
+      </KeyboardAvoidingView>
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create( {
+  input: {
+    backgroundColor: ThemeColours.cultured,
+    fontSize: 16,
+    padding: 5,
+    borderRadius: 4,
+  },
+  button: {
+    marginVertical: 15,
+    backgroundColor: ThemeColours.cerise,
+    padding: 10,
+    borderRadius: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: ThemeColours.cerulean,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: ThemeColours.cultured,
+    textAlign: 'center',
+  },
+  inner: {
+    width: 300,
+    marginBottom: 90,
+  },
+  kb: {
+    flex: 1,
+  }
+})
