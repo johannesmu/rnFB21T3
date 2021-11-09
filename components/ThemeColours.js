57f0f68cd0ae4b2b2ffe6098e39b72922a138eba
johannes <jmuljana@gmail.com>
2021-11-10 01:39:30 +1100

    added new colors

--- components/ThemeColours.js
+++ components/ThemeColours.js
@@ -1,6 +1,8 @@
 export const ThemeColours = {
   cultured: 'hsla(0, 0%, 94%, 1)',
+  culturedTransparent: 'hsla(0, 0%, 94%, 0.5)',
   cerulean: 'hsla(228, 63%, 51%, 1)',
+  ceruleanLight: 'hsla(228, 63%, 71%, 1)',
   turquoise: 'hsla(173, 63%, 51%, 1)',
   black: 'hsla(0, 0%, 3%, 1)',
   cerise: 'hsla(345, 63%, 51%, 1)',
