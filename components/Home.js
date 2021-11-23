05f259df7d7a39ea401491bb8b344d7c0fc7f5cc
johannes <jmuljana@gmail.com>
2021-11-23 22:40:46 +1100

    update to test data write

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from '@react-navigation/native';
+import { ThemeColours } from './ThemeColours';
 
 export function Home ( props ) {
   const navigation = useNavigation()
@@ -11,9 +12,21 @@ export function Home ( props ) {
    }
   }, [props.auth])
 
+  const data = { time: new Date().getTime(), user: 'test'}
+
   return(
     <View>
       <Text>Home</Text>
+      <TouchableOpacity style={styles.button} onPress={ () => { props.add('cities', data ) }}>
+        <Text>Add something</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: ThemeColours.turquoise,
+    padding: 10,
+  },
+})
