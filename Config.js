24f41e74748ed75d168ae12f00df467bd92b6eeb
johannes <jmuljana@gmail.com>
2021-11-23 22:39:21 +1100

    created new Firebase project

--- Config.js
+++ Config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyATxusSxp8STkXjZUq-mjcFJTCzpDJAfM0",
-  authDomain: "rnfirebase-6595f.firebaseapp.com",
-  projectId: "rnfirebase-6595f",
-  storageBucket: "rnfirebase-6595f.appspot.com",
-  messagingSenderId: "474956507607",
-  appId: "1:474956507607:web:0752b692918fc36c460392",
-  measurementId: "G-Q9F6GZN297"
+  apiKey: "AIzaSyBFjabl9JWaBj_AItmq9Oa5YI-WroAVRLU",
+  authDomain: "rnfb2-9893f.firebaseapp.com",
+  projectId: "rnfb2-9893f",
+  storageBucket: "rnfb2-9893f.appspot.com",
+  messagingSenderId: "501288828614",
+  appId: "1:501288828614:web:b3512393e17e069457e1c7",
+  measurementId: "G-QN09RRJTR3"
 }
